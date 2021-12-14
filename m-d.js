5a3bc4f542ad239b73716effeecb88c841c1a362
sohelTanbir <you@example.com>
2021-12-14 11:12:27 +0600

    Learned Material Design

--- m-d.js
+++ m-d.js
@@ -1,1 +1,9 @@
 mdc.ripple.MDCRipple.attachTo(document.querySelector('.foo-button'));
+import {MDCBanner} from '@material/banner';
+const banner = new MDCBanner(document.querySelector('.mdc-banner'));
+import {MDCRipple} from '@material/ripple';
+
+const selector = '.mdc-button, .mdc-icon-button, .mdc-card__primary-action';
+const ripples = [].map.call(document.querySelectorAll(selector), function(el) {
+  return new MDCRipple(el);
+});
