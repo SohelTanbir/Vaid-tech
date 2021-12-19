88279c34483f9bf23e73233a123171989bd9672a
sohelTanbir <you@example.com>
2021-12-19 11:12:43 +0600

    Learned JavaScript outpute Methods

--- practice.js
+++ practice.js
@@ -1,1 +1,9 @@
 // welcome to practice JavaScript Programming World
+
+//output in javaScript
+const message = 'I am a simple text message';
+
+// document.write(message);
+// console.log('output by console.log method');
+// window.alert("output by window.alert method");
+// window.print("output by window.print method")
