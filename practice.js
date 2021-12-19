9432f55c5695544bb57ad5a8bb14a6b6683d8a25
sohelTanbir <you@example.com>
2021-12-19 15:09:31 +0600

    learn operatior and let const and var keyword different

--- practice.js
+++ practice.js
@@ -1,9 +1,52 @@
 // welcome to practice JavaScript Programming World
 
-//output in javaScript
+// 1. output in javaScript
 const message = 'I am a simple text message';
 
-// document.write(message);
+ // document.write(message);
 // console.log('output by console.log method');
 // window.alert("output by window.alert method");
-// window.print("output by window.print method")
+// window.print("output by window.print method");
+
+// 2. JavaScript Statements
+const statement = 10+5-5*2;
+// console.log(statement);
+var firstName ="Sohel";
+var lastName = "Rana";
+// console.log('fullName is = ', firstName +' '+lastName);
+
+// 3. comments in JavaScript
+/*
+    I am a multi line comment
+*/
+// I am a single line comment
+
+// 4. JavaScript Variables
+// there are 3 way to declare variable in Javascript
+var one = 'declare variable with var  that is a global scope variable';
+let two = 'declare variable with let keyword that is a block scope variable';
+const three = 'declare variable with const keyword that is a block constant scope variable';
+
+ var age = 21;
+ var age  // it will not lose it's value
+
+ const PI =3.1416;
+
+ // use of const array
+const array1 = ['name1', 'name2','name3', 'name4'];
+array1.name5;
+// console.log(array1);
+array1['name4'] = 'name5'
+// console.log(array1);
+// 5. operator in JavaScript
+// arithmetic operator
+const a = 10;
+const b = 20;
+const sum = a+b;
+const sub = a-b;
+const divide = a/b;
+const remainder = a%b;
+
+// increment operator
+let number = 10;
+number++
