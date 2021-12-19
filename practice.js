caeb85422fba16c22cf1c37f7e640669c29bf74a
sohelTanbir <you@example.com>
2021-12-19 19:32:50 +0600

    learned JavaScript function, object, string, string methods

--- practice.js
+++ practice.js
@@ -50,3 +50,59 @@ const remainder = a%b;
 // increment operator
 let number = 10;
 number++
+// let x = 50
+// let y = 6;
+//  x+=y;
+//  const result =x+y;
+// const fullName =  firstName+=lastName;
+// console.log(x.length?'welcome':y)
+// console.log('remainder is = ', x%y)
+let x = 5;
+let z = x ** 2;   
+// console.log('exponentiation = ', z)
+// console.log(Math.pow(x, 2))
+
+// 6. JavaScript Functions
+
+function greeting(name){
+    console.log('Welcome '+ name)
+    return 0
+    console.log('Welcome '+ name)
+}
+
+
+// 7. JavaScript Object
+const person = {
+    name:'sohelrana',
+    height:'5.2" ',
+    weight:58,
+    eyeColor:'Black',
+    age:function(age){
+        return typeof(age) == 'number'?age:'not number'
+    }
+}
+// console.log(person.age(50))
+
+// 7. JavaScript Event
+function IncrementUser(){
+    let countUser = parseInt(document.querySelector('.add-user').innerText);
+    document.querySelector('.add-user').innerText = countUser+1
+}
+function decrementUser(){
+    let countUser = parseInt(document.querySelector('.add-user').innerText);
+    if(countUser!=0)
+        document.querySelector('.add-user').innerText = countUser-1
+}
+
+// 8.  JavaScript String
+let txt = "We Love Bangladesh ";
+console.log('length = ', txt.length)
+// 8.  JavaScript String Methods
+const sliceString = txt.substring(7,0);
+const newTxt = txt.replace(/bangladesh/i,"JavaScript");
+
+// console.log(newTxt.toUpperCase())
+// console.log(newTxt.charAt())
+// split method
+let splitArray = txt.trim().split(" ");
+console.log(splitArray)
