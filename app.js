2c5ad203837a5e6b8278616ff765137e29add0c5
SohelTanbir <soheltanbir19@gmail.com>
2021-12-28 22:28:48 +0600

    countDown is working properly

--- app.js
+++ app.js
@@ -5,38 +5,37 @@ function fetchData() {
     http.open("get", "https://date.nager.at/api/v2/publicholidays/2020/US");
     http.send();
     return http.onload = function () {
-       return  JSON.parse(http.responseText)
+        return JSON.parse(http.responseText)
     }
 }
 let apiData = fetchData();
 
 // show final output in UI 
-function displayData (inputDate){
+function displayData(inputDate) {
     // user input data ready for final calculation
     const userData = {
-        date:'',
-        month:'',
-        year:'',
-        hours:'',
-        minutes:''
+        date: '',
+        month: '',
+        year: '',
+        hours: '',
+        minutes: ''
     }
     // split user input data
     const splitDate = inputDate[2].split("T");
     const splitTime = splitDate[1].split(":");
 
-// set user input data in a userData object
+    // set user input data in a userData object
     userData.hours = splitTime[0]
     userData.minutes = splitTime[1]
     userData.year = inputDate[0];
     userData.month = inputDate[1];
-    userData.date =splitDate[0]
+    userData.date = splitDate[0]
 
     // get final output from calculation method
-    const api_data=  calculation();
+    const api_data = calculation();
 
     // pass data to countdown method
-    const output =  countDown(userData, api_data);
-
+    const output = countDown(userData, api_data);
     // select table id to show output in table
     const table = document.getElementById("table");
     let tr = `
@@ -59,71 +58,73 @@ function displayData (inputDate){
     }
     table.innerHTML = tr;
 
-} 
+}
 
 // count down date
-function countDown(inputData ,api_data ){
-
+function countDown(inputData, api_data) {
+    let days = inputData.date - api_data.date;
     let hours = Math.abs(inputData.hours - api_data.hours);
     let minutes = inputData.minutes - api_data.minutes;
     let seconds = 59;
-const stopCount = setInterval(() => {
+
     if (inputData.month > 1 && inputData.month > api_data.month) {
         days = (inputData.date - api_data.date);
         days += (inputData.month - api_data.month) * 30;
     } else {
         days = inputData.date - api_data.date;
     }
-    if(seconds > 0){
-        seconds = seconds-1
-    }else if( seconds == 0){
-        seconds = 59;
-        if (minutes > 1) {
-            if (minutes == 1) {
-                minutes = 59;
+
+    const stopCount = setInterval(() => {
+        if (seconds > 0) {
+            seconds = seconds - 1
+        } else if (seconds == 0) {
+            if (hours == 0 && minutes == 0) {
+                seconds = 0;
             } else {
-                minutes = minutes - 1;
-            }
-        } else if (minutes == 1) {
-            if (days >= 0 && hours > 0) {
-                minutes = 59;
-                minutes = minutes - 1;
-            } else {
-                minutes = 0
+                seconds = 59;
             }
 
-            if (hours > 1) {
-                hours = hours - 1;
-            } else if (hours == 1) {
-                if (days > 1 && hours == 1) {
-                    hours = 23
+            if (minutes > 1) {
+                if (minutes == 1) {
+                    minutes = 59;
                 } else {
-                    hours = 0;
-                    days = days - 1;
-                    console.log('1', days)
+                    minutes = minutes - 1;
                 }
-                if (days > 0 && hours == 1) {
-                    days = days - 1;
+            } else if (minutes == 1) {
+                if (days >= 0 && hours > 0) {
+                    minutes = 59;
+                    minutes = minutes - 1;
+                } else {
+                    minutes = 0
+                }
+                if (hours >= 1) {
+                    hours = hours - 1
+                    if (days > 0 && hours == 1) {
+                        hours = 23;
+                    }
+                }
+                if(days > 0 && hours == 0){
+                    days = days -1;
+                    hours = 23;
                 }
             }
         }
-    }
-    document.getElementById("demo").innerHTML = ` ${days} days ${hours} hours ${minutes} min ${seconds} Second`
-}, 1); 
+        document.getElementById("demo").innerHTML = ` ${days} days ${hours} hours ${minutes} min ${seconds} Second`
+    }, 1);
 
-
-return {days, hours, minutes, seconds}
+    console.log(days)
+    return { days, hours, minutes, seconds }
 }
 
 
 // calculation date and time 
-function calculation(){
+function calculation() {
     const apiDate = {
-        date:'',
-        month:'',
-        year:'',
-        hours:'24',
-        minutes:'00'
+        date: '',
+        month: '',
+        year: '',
+        hours: '24',
+        minutes: '00'
     }
     const trimDate = apiData()[0].date.toString().split("-");
     apiDate.year = trimDate[0];
@@ -141,5 +142,6 @@ document.getElementById("submit-btn").addEventListener("click", function (e) {
     } else {
         alert("Please Select a Date!")
     }
+    document.getElementById("date").value = '';
 })
 
