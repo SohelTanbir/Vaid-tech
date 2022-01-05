d946487d30a771055c4f3cca90f3839ffe34dbd1
SohelTanbir <soheltanbir19@gmail.com>
2022-01-05 15:52:20 +0600

    learn jquery set and get method

--- main.js
+++ main.js
@@ -57,13 +57,79 @@ $(document).ready(function(){
 $(".hide").click(function(){
     $("table").toggle("slow");
 })
+// fade effect
+// $("#effects").click(function(){
+//     $(".div1").fadeOut()
+// })
+// $("#effects").click(function(){
+//     $(".div2").fadeOut(1000)
+// })
+// $("#effects").click(function(){
+//     $(".div2").fadeToggle(2000)
+// })
+// $("#effects").click(function(){
+//     $(".div2").fadeTo(2000, .3)
+// })
 
+/// slide method
+$(".slide1").click(function(){
+    $(".panel1").slideUp(1000)
+})
+$(".slide2").click(function(){
+    $(".panel2").slideDown(1000)
+})
+$(".slide3").click(function(){
+    $(".panel3").slideToggle(1000)
+})
 
+// animation method
+$("#move").click(function(){
+    $(".div2").animate({
+        left:'40%',
+        height:'300px'
+    }, 2000).animate({width:'400px',height:'100px'})
+})
+$("#stop").click(function(){
+    $(".div2").stop()
+})
 
+// get  method jquery
+$("#gets").click(function(){
+    var txt = $("h3").text();
+    alert('get tet = '+ txt)
+})
+// $("#gethtml").click(function(){
+//    var html = $("#test").html();
+//    alert(html)
+// })
+$("#gethtml").click(function(){
+   var html = $("#test").val();
+   alert(html)
+})
 
-
-
-
+$("#inValue").click(function(){
+   var val = $("#inputs").val();
+   alert(val)
+})
+$("#getattr").click(function(){
+   var attr = $("#test").attr("title");
+   alert(attr)
+})
+// set method jquery
+$("#gets2").click(function(){
+    var settxt = $("h3").text("text set by javascript");
+    alert('set text in h3')
+})
+$("#gets2").click(function(){
+    var settxt = $("h3").text("text set by javascript");
+    alert('set text in h3')
+})
+$("#setinputeVal").click(function(){
+   $("#inputs2").val("value set by javascript");
+})
+$("#setattr").click(function(){
+   $("img").attr('src', 'https://images.unsplash.com/photo-1520209759809-a9bcb6cb3241?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1nfGVufDB8fDB8fA%3D%3D&w=1000&q=80');
+})
 
 
 
