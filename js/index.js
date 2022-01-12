d19ef8f7cb0b85656c43c1252b6b4eccb24dadec
SohelTanbir <soheltanbir19@gmail.com>
2022-01-12 19:28:33 +0600

    add event calender and UI design completed

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
-console.log("Event Calender is working properly");
+// get user input value
+const submitBtn = document.getElementById("subBtn")
+submitBtn.addEventListener("click", function(){
+    alert("data submited")
+})
+
+
+
+// call calender method 
+const calenderElement = document.getElementById("calender");
+// custome events
+const events = [
+    {
+        'Date': new Date(2022, 0, 5),
+        'Title':'Happy Birthday',
+    },
+    {
+        'Date': new Date(2022, 0, 10),
+        'Title':'Exam will be start',
+    },
+    {
+        'Date': new Date(2022, 0, 15),
+        'Title':'Travel with airplane',
+    },
+]
+const settings = {};
+
+// call the calender method to work with calender 
+caleandar(calenderElement, events, settings);
