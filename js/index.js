7db0094e4708bf3de5565f8cb73eda2f095a1bbe
SohelTanbir <soheltanbir19@gmail.com>
2022-01-22 12:16:44 +0600

    Event calendar is completed. user can add, delete and update event now

--- js/index.js
+++ js/index.js
@@ -18,6 +18,7 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
     let id = 1;
+    let publicId = ''
     submitBtn.addEventListener("click", function (e) {
         e.preventDefault();
         const inputData = handleInput();
@@ -37,42 +38,69 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
     var calendarEl = document.getElementById('calendar');
+
     var calendar = new FullCalendar.Calendar(calendarEl, {
         // include any option here by fullcalendar rules
         eventClick: function (eventId) {
             $("#update").modal('show');
-
+            // select a specific event by id and show in the update modal
             const findEvent = calendar.getEventById(eventId.event._def.publicId);
             const eventTitle = findEvent._def.title;
-            const publicId = findEvent._def.publicId;
+            publicId = findEvent._def.publicId;
             const dateString = findEvent._instance.range.start;
             const date = dateString.toLocaleString().split(",");
             const udate = date[0].split("/");
             const stringDate = `${udate[2]}-${udate[0] < 10 ? '0' + udate[0] : udate[0]}-${udate[1] < 10 ? '0' + udate[1] : udate[1]}`;
             document.getElementById("utitle").value = eventTitle;
             document.getElementById("udescription").value = eventTitle;
-            document.getElementById("udate").defaultValue = stringDate;
+            document.getElementById("updateDate").value = stringDate;
 
 
-
-            // delete event
-            document.getElementById("delete").addEventListener("click", function (e) {
+            // update event
+            document.getElementById("updateBtn").addEventListener("click", function (e) {
                 e.preventDefault();
-                const deleteEvent = window.confirm("Do you want to delete the Event?");
-                if (deleteEvent) {
-                    const evnt = calendar.getEventById(publicId);
-                    evnt.remove();
-                    alert("Event Deleted Successfully");
-                    $("#update").modal('hide');
+                const Utitle = document.getElementById("utitle").value.trim();
+                const description = document.getElementById("udescription").value;
+                const UdateArr = document.getElementById("updateDate").value.split("-");
+                const Uyear = UdateArr[0];
+                const Umonth = UdateArr[1] - 1;
+                const Udate = UdateArr[2];
+
+                if (Utitle.length > 0 && UdateArr.length > 0) {
+                    calendar.addEvent({
+                        id: publicId,
+                        title: Utitle,
+                        start: new Date(`${Uyear}`, `${Umonth}`, `${Udate}`)
+                    });
+
+                    if (publicId) {
+                        const delEvent = calendar.getEventById(publicId);
+                        delEvent?.remove();
+                    }
+                    document.getElementById("utitle").value = '';
+                    document.getElementById("udescription").value = '';
+                    document.getElementById("updateDate").value = '';
+                    $("#update").modal("hide");
                 }
             });
-
-
-
         }
     });
-    // add new event 
-    function addEvents(event) {
+
+
+// delete event
+document.getElementById("delete").addEventListener("click", function (e) {
+        e.preventDefault();
+        if (publicId) {
+            const delEvent = calendar.getEventById(publicId);
+            delEvent?.remove();
+            document.getElementById("utitle").value = '';
+            document.getElementById("udescription").value = '';
+            document.getElementById("updateDate").value = '';
+            $("#update").modal("hide");
+        }
+});
+// add new event 
+function addEvents(event) {
         calendar.addEvent(event, true)
         calendar.render();
         alert("Event Added Successfully");
