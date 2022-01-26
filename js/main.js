c25d674d2b7824f2e869cdfaa7e9cf29e5d2f43d
SohelTanbir <soheltanbir19@gmail.com>
2022-01-26 10:45:26 +0600

    making started bar chart to show student subject marks

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// making 3 type of chart using chart.js
+
+// bar chart to show student subject marks
+const barChart = document.getElementById("barChart");
+
+new Chart("barChart", {
+    type:"bar",
+    data:{},
+    options:{
+        
+    }
+})
+
