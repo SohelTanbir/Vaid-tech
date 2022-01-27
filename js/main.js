eb086683ed336757ccfa6772dc0fd11d44ebc94d
SohelTanbir <soheltanbir19@gmail.com>
2022-01-27 12:04:41 +0600

    created bar chart with dynamic api data of USA population statistics

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ fetch("https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/stats?country="
 })
 .then(response => response.json())
 .then(covidData => {
-    for(let i = 10; i<50; i++){
+    for(let i = 10; i<60; i++){
        if(!countries.includes(covidData.data.covid19Stats[i].country)){
         countries.push(covidData.data.covid19Stats[i].country);
         deaths.push(covidData.data.covid19Stats[i].deaths);
@@ -24,46 +24,57 @@ fetch("https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/stats?country="
 	console.error(err);
 });
 
-
-
-
-
-
-
+// get population data of USA
+const years = [];
+const populations = [];
+fetch("https://datausa.io/api/data?drilldowns=Nation&measures=Population")
+    .then(response => response.json())
+    .then(populationObj => {
+        for(let i = 0; i<populationObj.data.length; i++){
+            if(!years.includes(populationObj.data[i].Year)){
+                years.push(populationObj.data[i].Year);
+                populations.push(populationObj.data[i].Population);
+                nation = populationObj.data[i].Nation
+            }
+         }
+      barChartPopulation( populationObj.data[0].Nation)
+    })
 
 // bar chart to show student subject marks
 const barChart = document.getElementById("barChart");
-const semester = ["1st Semester", "2nd Semester", "3rd Semester", "4th Semester", "5th Semester", "6th Semester", "7th Semester", "8th Semester"];
-const marks = [45, 65, 77, 60, 90, 30, 89, 40]
-new Chart("barChart", {
-    type: "bar",
-    data: {
-        labels: semester,
-        datasets: [{
-            label: 'Bar chart',
-            data: marks,
-            backgroundColor: ["orange", "blue", "red", "skyblue", "tomato", "green", "pink", "salmon"],
-            index: 1
-        }]
-    },
-    options: {
-        plugins: {
-            title: {
-                display: true,
-                text: "Student Subject Marks in 2022"
-            }
+
+// population statistics of a country 
+function barChartPopulation(country){
+
+    new Chart("barChart", {
+        type: "bar",
+        data: {
+            labels: years.reverse(),
+            datasets: [{
+                label: 'Bar chart',
+                data: populations,
+                backgroundColor: ["orange", "blue", "red", "skyblue", "tomato", "green", "pink", "salmon"],
+                index: 1
+            }]
         },
-        scales: {
-            y: {
-                suggestedMin: 0,
-                suggestedMax: 100
+        options: {
+            plugins: {
+                title: {
+                    display: true,
+                    text: `Population Statistics of ${country}`
+                }
+            },
+            scales: {
+                y: {
+                    suggestedMin: 0,
+                    suggestedMax: 100
+                }
             }
         }
-    }
-});
+    });
+}
 // line chart to show covid-19 statistics
 const lineChart = document.getElementById("lineChart");
-
 
 function ShowLineChart(){
         new Chart("lineChart", {
@@ -87,19 +98,10 @@ function ShowLineChart(){
                     text: "Covid-19 Statistics in the world"
                 },
                 tooltip:{
-                    afterFooter:["sohel"]
+                    footer:['sohel', "rana"]
                 }
             }
         }
     })
     
 }
-
-
-
-
-
-
-
-
-
