642c97e8e537f4a618c2f58f14abcee245a41187
SohelTanbir <soheltanbir19@gmail.com>
2022-01-30 11:56:54 +0600

    show covi 19 statistis by select a country

--- js/main.js
+++ js/main.js
@@ -1,92 +1,89 @@
 // making 3 type of chart using chart.js
 // handle input change
 // covid-19 statistics
-const deaths = [];
+let deaths = [];
 const months = [];
-let isdestroy = 0;
-function handleCountryChange(){
-    const  country = document.getElementById("country").value;
-     fetch(`https://raw.githubusercontent.com/SohelTanbir/API-Data/master/covid19-deaths-${country.length? country:'bangladesh'}.json`)
-    .then(response => response.json())
-    .then(covidData => {
-        console.log(covidData);
-        for(let i =0; i<covidData.length; i++){
-           if(!months.includes(covidData[i].month)){
-            months.push(covidData[i].month);
-           }
-           deaths.push(covidData[i].deaths);
-        }
-        isdestroy +=1;
-        LineChart(); 
-    })
-    .catch(err => {
-        console.error(err);
-    });
-    
+const country = [];
+function handleCountryChange() {
+   let country = document.getElementById("country").value;
+    fetch(`https://raw.githubusercontent.com/SohelTanbir/API-Data/master/covid19-deaths-${country}.json`)
+        .then(response => response.json())
+        .then(covidData => {
+            for (let i = 0; i < covidData.length; i++) {
+                if (!months.includes(covidData[i].month)) {
+                    months.push(covidData[i].month);
+                }
+                deaths.push(covidData[i].deaths);
+            }
+            document.getElementById("lineChart").remove();
+            document.querySelector(".select-country").innerHTML =`
+            <canvas id="lineChart" style="width: 100%; max-width: 1000px; height: 400px;"></canvas>
+            `;
+           LineChart(covidData[0].country);
+            deaths = []
+        })
+        .catch(err => {
+            console.error(err);
+        });
 }
-
-// destroy chart 
-
+LineChart()
 // get population data of a country
 const years = [];
 const populations = [];
 fetch("https://raw.githubusercontent.com/SohelTanbir/API-Data/master/population-statistics.json")
     .then(response => response.json())
     .then(populationObj => {
-        for(let i = 0; i<populationObj.length; i++){
-            if(!years.includes(populationObj[i].year)){
+        for (let i = 0; i < populationObj.length; i++) {
+            if (!years.includes(populationObj[i].year)) {
                 years.push(populationObj[i].year);
                 populations.push(populationObj[i].population);
             }
-         }
-      barChartPopulation( populationObj[0].country)
+        }
+        barChartPopulation(populationObj[0].country)
     })
 
 const divisions = [];
 const coordinates = [];
 fetch("https://raw.githubusercontent.com/SohelTanbir/API-Data/master/division.json")
-.then(res => res.json())
-.then(divisionData => {
-    for(let i = 0; i<divisionData.length; i++){
-        if(!divisions.includes(divisionData[i].division)){
-            divisions.push(divisionData[i].division);
-            coordinates.push(parseInt(divisionData[i].size));
+    .then(res => res.json())
+    .then(divisionData => {
+        for (let i = 0; i < divisionData.length; i++) {
+            if (!divisions.includes(divisionData[i].division)) {
+                divisions.push(divisionData[i].division);
+                coordinates.push(parseInt(divisionData[i].size));
+            }
         }
-     }
-     divisionOfBD()
-});
+        divisionOfBD()
+    });
 
 // line chart to show covid-19 statistics
 const lineChart = document.getElementById("lineChart");
-
-function LineChart(){
-     const linechart =   new Chart("lineChart", {
+function LineChart(country) {
+    const linechart = new Chart("lineChart", {
         type: "line",
         data: {
             labels: months,
             datasets: [
                 {
-                fill:false,
-                label:"Line Chart",
-                backgroundColor: "skyblue",
-                data:deaths,
-                borderColor: 'blue',
-                pointBorderWidth:5,
-            }
-        ]
+                    fill: false,
+                    label: "Line Chart",
+                    backgroundColor: "skyblue",
+                    data: deaths,
+                    borderColor: 'blue',
+                    pointBorderWidth: 5,
+                }
+            ]
         },
         options: {
             plugins: {
                 title: {
                     display: true,
-                    text: "Covid-19 Statistics 2020 and 2021 of Bangladesh"
+                    text: `Covid-19 Statistics 2020 and 2021 of ${country}`
                 },
             }
         }
     })
-    if(isdestroy > 1){
-        linechart.destroy();
-    }
+    return linechart;
 }
 
 
@@ -94,7 +91,7 @@ function LineChart(){
 const barChart = document.getElementById("barChart");
 
 // population statistics of a country 
-function barChartPopulation(country){
+function barChartPopulation(country) {
     new Chart("barChart", {
         type: "bar",
         data: {
@@ -112,9 +109,9 @@ function barChartPopulation(country){
                     display: true,
                     text: `Population Statistics of ${country}`
                 },
-                tooltip:{
+                tooltip: {
                     callbacks: {
-                        beforeLabel: function(context) {
+                        beforeLabel: function (context) {
                             if (context.parsed.y !== null) {
                                 beforeLabel = "Total population";
                             }
@@ -133,22 +130,22 @@ function barChartPopulation(country){
     });
 }
 // doughnut chart 
-function divisionOfBD(){
+function divisionOfBD() {
     new Chart("doughnutChart", {
-        type:"doughnut",
-        data:{
-            labels:divisions,
-            datasets:[{
-                data:coordinates,
+        type: "doughnut",
+        data: {
+            labels: divisions,
+            datasets: [{
+                data: coordinates,
                 backgroundColor: ["orange", "blue", "red", "skyblue", "tomato", "green", "pink", "salmon"],
             }]
         },
-        options:{
-            plugins:{
-                display:false,
-                title:{
-                    display:true,
-                    text:"divisions size of Bangladesh"
+        options: {
+            plugins: {
+                display: false,
+                title: {
+                    display: true,
+                    text: "divisions size of Bangladesh"
                 }
             }
         }
