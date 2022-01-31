325b84457888b3f3f6a113c3dee7a6797faf02ac
SohelTanbir <soheltanbir19@gmail.com>
2022-01-31 11:11:56 +0600

    add event listener on crop button

--- js/main.js
+++ js/main.js
@@ -6,26 +6,30 @@ window.addEventListener("load", function () {
 
   // image uploader
   function uploader() {
-    if (this.files && this.files[0] && this.files[0].type =='image/jpeg' || this.files[0].type =='image/png') {
+    if (this.files && this.files[0] && this.files[0].type == 'image/jpeg' || this.files[0].type == 'image/png') {
 
       document.getElementById("img").style.display = "block"
       const img = document.getElementById("img");
-      img.onload = ()=>{
-        URL.revokeObjectURL(img.src); 
+      img.onload = () => {
+        URL.revokeObjectURL(img.src);
       }
       img.src = URL.createObjectURL(this.files[0]);
-    
+
       // hide input field after upload image
-      this.style.display ='none'
-    }else{
+      this.style.display = 'none'
+    } else {
       alert("Sorry! only image allow")
     }
 
-// Relaod window 
-document.querySelector(".reload").addEventListener("click", function(){
-  window.location.reload();
-})
+    // Relaod window 
+    document.querySelector(".reload").addEventListener("click", function () {
+      window.location.reload();
+    });
 
+  // crop or select a part of upload image
+document.querySelector(".crop").addEventListener("click", function(){
+    alert("image cropped")
+})
 
 
   };
