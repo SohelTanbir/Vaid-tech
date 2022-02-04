83124cc88f4cb82ac20008577a84a29ef2126609
SohelTanbir <soheltanbir19@gmail.com>
2022-02-04 20:22:45 +0600

    solve problem when crop image

--- js/main.js
+++ js/main.js
@@ -36,35 +36,43 @@ window.addEventListener("load", function () {
     let sw = 0;
     let sh = 0;
     const selectArea = document.getElementById("selectArea");
-    document.getElementById("img").addEventListener("mousedown", (e) => {
-      sx = e.clientX -20;
-      sy = e.clientY-100;
-      selectArea.style.visibility = 'visible';
-      document.getElementById("x2").innerHTML =`start x:${sx}`;
-      document.getElementById("y2").innerHTML = ` start y:${sy}`;
-      // set margin left and top
-      selectArea.style.marginLeft = `${sx}`+"px";
-      selectArea.style.marginTop = `${sy}`+"px";
-      document.getElementById("img").addEventListener("mousemove", handleMouseMove);
-    });
+    const overlay = document.querySelector(".overlay");
+    document.getElementById("img").addEventListener("mousedown", mouseDown);
+
+// handle mousedown event 
+function mouseDown(e){
+  sx = e.clientX -20;
+  sy = e.clientY-50
+  selectArea.style.visibility = 'visible';
+  overlay.style.display = 'block';
+  document.getElementById("x2").innerHTML =`start x:${sx}`;
+  document.getElementById("y2").innerHTML = ` start y:${sy}`;
+  // set margin left and top
+  selectArea.style.marginLeft = `${sx}`+"px";
+  selectArea.style.marginTop = `${sy}`+"px";
+  document.getElementById("img").addEventListener("mousemove", handleMouseMove);
+}
 // handle mousemove event
 function handleMouseMove(e){
+
   sw = e.clientX;
   sh = e.clientY;
   document.getElementById("x").innerHTML =`${sw}x `;
-  document.getElementById("y").innerHTML = ` ${ sh}`;
+  document.getElementById("y").innerHTML = ` ${sh}`;
   selectArea.style.width = `${sw}` + "px";
   selectArea.style.height = `${sh}` + "px";
   document.getElementById("xw").innerHTML =`w:${sw}`;
   document.getElementById("xy").innerHTML = ` h:${sh}`;
+
 }
 // remove eventListener
 function removeEvent(){
-  document.getElementById("img").removeEventListener("mousemove", handleMouseMove)
+  document.getElementById("img").removeEventListener("mousemove", handleMouseMove);
+  // document.getElementById("img").removeEventListener("mousedown", mouseDown);
 }
 
-  document.getElementById("selectArea").addEventListener("mouseup", (e) => { removeEvent()});
-  document.getElementById("img").addEventListener("mouseup", (e) => { removeEvent() });
+  document.getElementById("selectArea").addEventListener("mouseup", removeEvent);
+  document.getElementById("img").addEventListener("mouseup", removeEvent);
 
     // crop or select a part of upload image
     document.querySelector(".crop").addEventListener("click", function () {
@@ -79,28 +87,24 @@ function removeEvent(){
       img.onload = function () {
       const imgWidth = img.width;
       const imgHeight = img.height;
-      console.log('W = ', imgWidth, 'H', imgHeight);
-      console.log('sx = ', sx);
-      console.log('sy = ', sy);
-
-      console.log('sw = ', sw);
-      console.log('sh = ', sh);
-
 
       sx =Math.floor( (sx/644)*imgWidth);
       sy = Math.floor((sy/350)*imgHeight);
-
+      const selectX = sw;
+      const selectY = sh;
       sw =Math.floor( (sw/644)*imgWidth);
       sh = Math.floor((sh/350)*imgHeight);
-    
-      
-        context.drawImage(img, sx, sy,sw,sh, 0, 0, 300, 200);
+
+        context.drawImage(img, sx, sy,sw,sh, 0, 0, selectX, selectY);
+        selectArea.style.backgroundColor ='#fff';
       }
     }
 
     // endline if condition
   };
 
+  
 
 
 });
+
