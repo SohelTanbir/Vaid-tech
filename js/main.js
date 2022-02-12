6304494b5530913d94e5e2bd97789cd91cdae869
SohelTanbir <soheltanbir19@gmail.com>
2022-02-12 19:15:03 +0600

    add all events listener to perfome user command

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 // making a simple editor 
 
+// add event to action user command
 document.getElementById("bold").addEventListener("click", textBold);
 document.getElementById("italic").addEventListener("click", textItalic);
 document.getElementById("underline").addEventListener("click", textUnderline);
@@ -8,32 +9,47 @@ document.getElementById("center").addEventListener("click", textCenter);
 document.getElementById("right").addEventListener("click", textRight);
 document.getElementById("justify").addEventListener("click", textJustify);
 
-
+// get selected text content
 function selectedText(){
     const text = window.getSelection();
     if(text){
-       document.getElementById("demo").innerHTML = text;
+       document.getElementById("editor-area").innerHTML = text;
     }
 }
-document.querySelector(".editor-area").addEventListener("mouseup", selectedText);
+// if text content available in editable div then user can text formatting 
+const isContent = document.querySelector(".editor-area").value;
+if(isContent){
+    document.querySelector(".editor-area").addEventListener("mouseup", selectedText);
+}
+
+// all action methods
 function textBold(){
-    document.getElementById("demo").classList.toggle("text-bold");
+    document.querySelector(".editor-area").classList.toggle("text-bold");
 }
 function textItalic(){
-    document.getElementById("demo").classList.toggle("text-italic");
+    document.querySelector(".editor-area").classList.toggle("text-italic");
 }
 function textUnderline(){
-    document.getElementById("demo").classList.toggle("text-underline");
+    document.querySelector(".editor-area").classList.toggle("text-underline");
 }
 function textLeft(){
-    document.getElementById("demo").classList.add("text-left");
+    document.querySelector(".editor-area").classList.add("text-left");
+    document.querySelector(".editor-area")?.classList?.remove("text-center");
+    document.querySelector(".editor-area")?.classList?.remove("text-right");
 }
 function textCenter(){
-    document.getElementById("demo").classList.add("text-center");
+    document.querySelector(".editor-area").classList.add("text-center");
+    document.querySelector(".editor-area").classList?.remove("text-left");
+    document.querySelector(".editor-area").classList?.remove("text-right");
 }
 function textRight(){
-    document.getElementById("demo").classList.add("text-right");
+    document.querySelector(".editor-area").classList.add("text-right");
+    document.querySelector(".editor-area").classList?.remove("text-center");
+    document.querySelector(".editor-area").classList?.remove("text-left");
 }
 function textJustify(){
-    document.getElementById("demo").classList.toggle("text-justify");
+    document.querySelector(".editor-area").classList.toggle("text-justify");
+    document.querySelector(".editor-area").classList?.remove("text-center");
+    document.querySelector(".editor-area").classList?.remove("text-left");
+      document.querySelector(".editor-area")?.classList?.remove("text-right");
 }
