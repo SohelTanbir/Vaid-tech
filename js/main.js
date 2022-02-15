06d5dc7a750e01c9d75abf7d87f737a53138569e
SohelTanbir <soheltanbir19@gmail.com>
2022-02-15 18:20:20 +0600

    image full width view featured added depend on selected image

--- js/main.js
+++ js/main.js
@@ -1,5 +1,24 @@
 $(document).ready(function(){
 
+    $(".item img").click(function(){
+     // show full width image 
+     $("#fullWidth").css("visibility", "visible");
+     $("#overlay-bg").css("visibility", "visible");
+    //set image select image
+       const url = this.src;
+       $("#fullWidth").attr("src", url)
+
+
+    });
+
+
+    // hide full view image
+    $(".close").click(function(){
+     $("#fullWidth").css("visibility", "hidden");
+     $("#overlay-bg").css("visibility", "hidden");
+     
+    })
+
 
 
 
