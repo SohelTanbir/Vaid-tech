6c825080abcba40ef20da6a7285eb9c6c1930fcb
SohelTanbir <soheltanbir19@gmail.com>
2022-02-15 19:28:32 +0600

    add fontawsome icon on single gallery item to view full screen image and add css using jquery

--- js/main.js
+++ js/main.js
@@ -1,23 +1,26 @@
 $(document).ready(function(){
 
-    $(".item img").click(function(){
+ $(".item").click(function(){
      // show full width image 
-     $("#fullWidth").css("visibility", "visible");
+   $("#fullWidth").css({visibility:"visible",transform:'scale(1)'});
+   $("#fullWidth").animate({width:'80vw',height:'90vh'});
+
      $("#overlay-bg").css("visibility", "visible");
     //set image select image
-       const url = this.src;
-       $("#fullWidth").attr("src", url)
-
-
-    });
+       const url = this.children[0].src;
+      $("#fullWidth").attr("src", url)
+      });
 
 
     // hide full view image
-    $(".close").click(function(){
+$(".close").click(function(){
+       // hide 
      $("#fullWidth").css("visibility", "hidden");
      $("#overlay-bg").css("visibility", "hidden");
      
-    })
+     // remove width adn height
+     $("#fullWidth").animate({width:'0',height:'0'})
+});
 
 
 
