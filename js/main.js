2a720732cd356e5ba0237511aa59d2224796fe41
SohelTanbir <soheltanbir19@gmail.com>
2022-02-16 11:03:06 +0600

    add border around full width image and add left and right angel icon to slide image

--- js/main.js
+++ js/main.js
@@ -1,25 +1,29 @@
 $(document).ready(function(){
 
+
+
+
  $(".item").click(function(){
      // show full width image 
-   $("#fullWidth").css({visibility:"visible",transform:'scale(1)'});
-   $("#fullWidth").animate({width:'80vw',height:'90vh'});
+   $("#fullWidth").css({visibility:"visible"});
+   $("#fullWidth").animate({width:'80vw',height:'90vh'}).fadeIn();
 
+   // show overlay of whole window
      $("#overlay-bg").css("visibility", "visible");
+
     //set image select image
        const url = this.children[0].src;
-      $("#fullWidth").attr("src", url)
+      $("#fullWidth").attr("src", url);
       });
 
 
-    // hide full view image
+    // hide full view image and overlay
 $(".close").click(function(){
-       // hide 
      $("#fullWidth").css("visibility", "hidden");
      $("#overlay-bg").css("visibility", "hidden");
      
      // remove width adn height
-     $("#fullWidth").animate({width:'0',height:'0'})
+    //  $("#fullWidth").animate({width:'0',height:'0'})
 });
 
 
