db01d84917e90a3242316f423ccb01feb2c11fa0
SohelTanbir <soheltanbir19@gmail.com>
2022-03-21 11:21:11 +0600

    Floating action buttion

--- src/Components/MUI.jsx
+++ src/Components/MUI.jsx
@@ -3,8 +3,12 @@ import Button from '@mui/material/Button';
 import DeleteIcon from '@mui/icons-material/Delete';
 import SendIcon from '@mui/icons-material/Send';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
-import { Stack } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import EditIcon from '@mui/icons-material/Edit';
+import NavigationIcon from '@mui/icons-material/Navigation';
+import { Checkbox, Stack } from '@mui/material';
 import { IconButton } from '@mui/material';
+import { ButtonGroup, Fab } from '@mui/material';
 
 
 const MUI = () => {
@@ -13,7 +17,7 @@ const MUI = () => {
 
         <h2 className='text'>Material UI Basic Components</h2>
 
-        <Stack spacing={3} direction="row" flexWrap="wrap" justifyContent="space-between" alignItems="center">
+        <Stack spacing={3} direction="row" flexWrap="wrap">
 
             <Button variant="text" color="primary">btn variant text</Button>
 
@@ -38,7 +42,25 @@ const MUI = () => {
             <Button variant="outlined" color="primary" startIcon={<DeleteIcon/>}>Delete</Button>
 
             <Button variant="outlined" color="primary" endIcon={<SendIcon/>}>Send</Button>
+
             <IconButton color='primary'>   <AddShoppingCartIcon /> </IconButton>
+            <ButtonGroup variant="contained" aria-label="outlined primary button group">
+                <Button>One</Button>
+                <Button>Two</Button>
+                <Button>Three</Button>
+        </ButtonGroup>
+
+        </Stack>
+        <Stack spacing={3} direction="row" flexWrap="wrap">
+
+        <Checkbox  defaultChecked color="primary"  />
+        <Fab color="primary" aria-label="add" size='small'> <AddIcon /> </Fab>
+        <Fab color="primary" aria-label="add" size='medium'> <EditIcon /> </Fab>
+        <Fab variant="extended">
+        <NavigationIcon sx={{ mr: 1 }} />
+        Navigate
+      </Fab>
+
 
         </Stack>
 
