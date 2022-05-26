d35079276ddc081517b6078554d7c91b845afe90
SohelTanbir <soheltanbir19@gmail.com>
2022-05-26 17:57:24 +0600

    add clear timout method to stop setTimeout function when user dismiss the toast notification

--- js/main.js
+++ js/main.js
@@ -1,5 +1,29 @@
 $(document).ready(function(){
-    $("dismiss-icon").click(function(){
-        alert('closed');
-    })
+    let clearTimeId;
+    // dismiss toast
+    $(".dismiss-icon").click(function(){
+        $("#toast-container").css("left","100%");
+        // stop setTimeout function
+        if(clearTimeId){
+            clearTimeout(clearTimeId)
+        }
+        
+    });
+
+    $("#login-btn").click(toastOpen)
+
+// toast open
+function toastOpen(){
+    $("#toast-container").css("left","70%");
+    clearTimeId =  setTimeout(toastHide,5000);
+}
+
+
+// toast hide
+function toastHide(time){
+    $("#toast-container").css("left","100%")
+}
+
+
 })
+
