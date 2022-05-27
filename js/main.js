b34e0727cb619f4ed3ee4faa732bde5657d84e16
SohelTanbir <soheltanbir19@gmail.com>
2022-05-27 12:30:13 +0600

    add time over animation with calculation

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
         $("#toast-container").css("left","100%");
         // stop setTimeout function
         if(clearTimeId){
-            clearTimeout(clearTimeId)
+            clearTimeout(clearTimeId);
         }
         
     });
@@ -16,13 +16,33 @@ $(document).ready(function(){
 function toastOpen(){
     $("#toast-container").css("left","70%");
     clearTimeId =  setTimeout(toastHide,5000);
+
+    // time progreebar 
+    let time = 5;
+    let width = 280;
+    let perSecondWidth = 280/time
+const interValId = setInterval(()=>{
+    if(time >0){
+        time = time -1;
+        const percentWidth =( (perSecondWidth/width)*100)*time;
+        console.log(percentWidth);
+        $("#progrees").css({width:`${percentWidth}`})
+    }else{
+        clearInterval(interValId)
+    }
+ 
+}, 1000)
 }
 
 
 // toast hide
 function toastHide(time){
-    $("#toast-container").css("left","100%")
+    $("#toast-container").css("left","100%");
+    // set default width of progressbar
+    $("#progrees").css({width:'100%!important'})
 }
+
+
 
 
 })
